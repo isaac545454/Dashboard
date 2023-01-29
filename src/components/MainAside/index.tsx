3d5c7258b57b2942a1cfae5b270a0d577da2152a
isaac545454 <isaac.newton1221@gmail.com>
2023-01-29 08:34:35 -0300

    feat: add file mainHeader and mainAside

---
+++ src/components/MainAside/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import * as S from "./styles";
+
+const Aside: React.FC = () => {
+  return <div></div>;
+};
+
+export default Aside;
---
+++ src/components/MainHeader/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import * as S from "./styles";
+
+const Header: React.FC = () => {
+  return <div />;
+};
+
+export default Header;
