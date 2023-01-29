916eb5aa15f1a3fe89d533992609cc191645f3e5
isaac545454 <isaac.newton1221@gmail.com>
2023-01-29 12:51:53 -0300

    feat: add emoji header

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useMemo } from "react";
+import emoj from "../../utils/emoj";
+import Emoj from "../../utils/emoj";
 
 import * as S from "./styles";
 
 const Header: React.FC = () => {
-  return <S.Container>aaaaaaaaa</S.Container>;
+  const randomEmoj = useMemo(() => {
+    const indice = Math.floor(Math.random() * Emoj.length);
+    return emoj[indice];
+  }, []);
+  return (
+    <S.Container>
+      <h1>aa</h1>
+      <S.Profile>
+        <S.Welcome>Olá, {randomEmoj}</S.Welcome>
+        <S.UserName>Isaac Gomes Matos</S.UserName>
+      </S.Profile>
+    </S.Container>
+  );
 };
 
 export default Header;
--- src/components/MainHeader/styles.ts
+++ src/components/MainHeader/styles.ts
@@ -4,4 +4,18 @@ export const Container = styled.div`
   grid-area: MH;
   background-color: ${(props) => props.theme.colors.secondary};
   color: ${(props) => props.theme.colors.white};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10px;
+  border-bottom: 1px solid ${(props) => props.theme.colors.gray};
 `;
+
+export const Profile = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Welcome = styled.h3``;
+
+export const UserName = styled.span``;
